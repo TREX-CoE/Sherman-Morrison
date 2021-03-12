6012b9decfc324cc984b7d5b70424502c7386095
François Coppens <francois.coppens@uvsq.fr>
2021-03-12 15:47:47 +0100

    4x4 with 2 updates example is working again after adding update cycle 8169 dataset but with replacement updates instead of additive updates.

--- tests/QMCChem_dataset_test.f90
+++ tests/QMCChem_dataset_test.f90
@@ -9,7 +9,7 @@ program QMCChem_dataset_test
     real(c_double), dimension(:,:), allocatable :: Updates, U
     real(c_double), dimension(:,:), allocatable :: S, S_inv, S_inv_t
 
-    call Read_dataset("datasets/update_cycle_8169.dat", &
+    call Read_dataset("datasets/update_cycle_8169_repl.dat", &
                        cycle_id, &
                        dim, &
                        n_updates, &
